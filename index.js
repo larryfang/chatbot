ac5431101002af98fb3b59f273919f58fcea7d91
Larry Fang <larry.fang@auspost.com.au>
2017-03-12 10:33:04 +1100

    adding integration with facebook messenger

--- index.js
+++ index.js
@@ -21,6 +21,34 @@ app.get('/webhook/', function(req,res) {
     res.send("Wrong token");
 })
 
+app.post('/webhook/', function(req,res) {
+    let messaging_events =  req.body.entry[0].messaging_events;
+    for (let i=0; i < messaging_events.length; i++) {
+        let event = messaging_events[i]
+        let sender = event.sender.id;
+        if(event.message && event.message.text) {
+            let text = evetn.message.text
+            sendText(sender, "Text echo:" + text.substring(0,100))
+        }
+    }
+    res.sendStatus(200)
+
+})
+
+const token = "EAADAcQndBogBAIzs4Wnkn4D1duPED7ZAyBh5htLaKUnxb4Rj5D8OZCf7brvDzEQra27bzgELvnWZBCSuqSSHxtgGB22UTf19OimRrtehv5WowZAPVHJ2SA8jR2kPlkgTEQCXzuSVl0ZCuUoH0LaxBwvgoflFgpA8BnNZCyZBwkgKAZDZD"
+function sendText(sender, text) {
+    let messageData = { text: text};
+    request({
+        url: "https://graph.facebook.com/v2.6/me/messages",
+        qs: {access_token, token} ,
+        method: "POST",
+        json: {
+            receipt: {id: sender},
+            message: messageData
+        }
+    })
+}
+
 app.listen(app.get('port'), function () {
     console.log("Running: port")
 }
