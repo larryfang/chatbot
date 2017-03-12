038ddb243f09c5f7626f39f68939cf35fba440ec
Larry Fang <larry.fang@auspost.com.au>
2017-03-12 22:39:27 +1100

    try template

--- index.js
+++ index.js
@@ -37,13 +37,35 @@ app.post('/webhook/', function(req,res) {
 
 const token = "EAADAcQndBogBAIzs4Wnkn4D1duPED7ZAyBh5htLaKUnxb4Rj5D8OZCf7brvDzEQra27bzgELvnWZBCSuqSSHxtgGB22UTf19OimRrtehv5WowZAPVHJ2SA8jR2kPlkgTEQCXzuSVl0ZCuUoH0LaxBwvgoflFgpA8BnNZCyZBwkgKAZDZD"
 function sendText(sender, text) {
-    let messageData = {attachment: {
-            type: "image",
+    // let messageData = {attachment: {
+    //         type: "image",
+    //         payload: {
+    //             url: "https://auspost.com.au/mypost-business/assets/mypost-business-app/images/mypost-business-logo.svg"
+    //         }
+    //     }
+    // }
+    let messageData = {
+        attachment: {
+            type: 'template',
             payload: {
-                url: "https://auspost.com.au/mypost-business/assets/mypost-business-app/images/mypost-business-logo.svg"
+                template_type: 'generic',
+                elements: [
+                    {
+                        title: '24th Street',
+                        'subtitle': '43 mins, 9 cars. 58 mins, 9 cars. 73 mins, 9 cars.'
+                    },
+                    {
+                        title: 'Daly City',
+                        'subtitle': '43 mins, 9 cars. 58 mins, 9 cars. 73 mins, 9 cars. 1 min, 9 cars. 4 mins, 9 cars.'
+                    },
+                    {
+                        title: 'Millbrae',
+                        'subtitle': '8 mins, 4 cars. 23 mins, 4 cars. 38 mins, 4 cars. 13 mins, 5 cars.'
+                    }
+                ]
             }
         }
-    }
+    };
     // let messageData = { text: text};
     request({
         url: "https://graph.facebook.com/v2.6/me/messages",
@@ -54,6 +76,7 @@ function sendText(sender, text) {
             message: messageData
         }
     })
+    console.log()
 }
 
 app.listen(app.get('port'), function () {
